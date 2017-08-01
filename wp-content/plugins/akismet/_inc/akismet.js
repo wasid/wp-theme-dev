57d3d5f90f78297c2ccffc90a6f9220799608b31
Wasid Hossain <wasidmail@gmail.com>
2017-08-01 10:51:53 +0000

    updated to latest wp version and installed starter theme from underscores.me

--- wp-content/plugins/akismet/_inc/akismet.js
+++ wp-content/plugins/akismet/_inc/akismet.js
@@ -1,15 +1,8 @@
 jQuery( function ( $ ) {
-	$( 'a.activate-option' ).click( function(){
-		var link = $( this );
-		if ( link.hasClass( 'clicked' ) ) {
-			link.removeClass( 'clicked' );
-		}
-		else {
-			link.addClass( 'clicked' );
-		}
-		$( '.toggle-have-key' ).slideToggle( 'slow', function() {});
-		return false;
-	});
+	var mshotRemovalTimer = null;
+	var mshotSecondTryTimer = null
+	var mshotThirdTryTimer = null
+	
 	$('.akismet-status').each(function () {
 		var thisId = $(this).attr('commentid');
 		$(this).prependTo('#comment-' + thisId + ' .column-comment');
@@ -18,21 +11,10 @@ jQuery( function ( $ ) {
 		var thisId = $(this).attr('commentid');
 		$(this).insertAfter('#comment-' + thisId + ' .author strong:first').show();
 	});
-	$('#the-comment-list').find('tr.comment, tr[id ^= "comment-"]').find('.column-author a[title ^= "http://"]').each(function () {
-		var thisTitle = $(this).attr('title');
-			thisCommentId = $(this).parents('tr:first').attr('id').split("-");
 
-		$(this).attr("id", "author_comment_url_"+ thisCommentId[1]);
-
-		if (thisTitle) {
-			$(this).after(
-				$( '<a href="#" class="remove_url">x</a>' )
-					.attr( 'commentid', thisCommentId[1] )
-					.attr( 'title', WPAkismet.strings['Remove this URL'] )
-			);
-		}
-	});
-	$('.remove_url').live('click', function () {
+	akismet_enable_comment_author_url_removal();
+	
+	$( '#the-comment-list' ).on( 'click', '.akismet_remove_url', function () {
 		var thisId = $(this).attr('commentid');
 		var data = {
 			action: 'comment_author_deurl',
@@ -69,10 +51,9 @@ jQuery( function ( $ ) {
 		});
 
 		return false;
-	});
-	$('.akismet_undo_link_removal').live('click', function () {
+	}).on( 'click', '.akismet_undo_link_removal', function () {
 		var thisId = $(this).attr('cid');
-		var thisUrl = $(this).attr('href').replace("http://www.", "").replace("http://", "");
+		var thisUrl = $(this).attr('href');
 		var data = {
 			action: 'comment_author_reurl',
 			_wpnonce: WPAkismet.comment_author_url_nonce,
@@ -91,46 +72,85 @@ jQuery( function ( $ ) {
 				if (response) {
 					// Add "x" link
 					$("a[commentid='"+ thisId +"']").show();
-					// Show link
-					$("#author_comment_url_"+ thisId).removeClass('akismet_undo_link_removal').html(thisUrl);
+					// Show link. Core strips leading http://, so let's do that too.
+					$("#author_comment_url_"+ thisId).removeClass('akismet_undo_link_removal').text( thisUrl.replace( /^http:\/\/(www\.)?/ig, '' ) );
 				}
 			}
 		});
 
 		return false;
 	});
-	$('a[id^="author_comment_url"], tr.pingback td.column-author a:first-of-type').mouseover(function () {
-		var wpcomProtocol = ( 'https:' === location.protocol ) ? 'https://' : 'http://';
-		// Need to determine size of author column
-		var thisParentWidth = $(this).parent().width();
-		// It changes based on if there is a gravatar present
-		thisParentWidth = ($(this).parent().find('.grav-hijack').length) ? thisParentWidth - 42 + 'px' : thisParentWidth + 'px';
-		if ($(this).find('.mShot').length == 0 && !$(this).hasClass('akismet_undo_link_removal')) {
-			var self = $( this );
-			$('.widefat td').css('overflow', 'visible');
-			$(this).css('position', 'relative');
-			var thisHref = $.URLEncode( $(this).attr('href') );
-			$(this).append('<div class="mShot mshot-container" style="left: '+thisParentWidth+'"><div class="mshot-arrow"></div><img src="//s0.wordpress.com/mshots/v1/'+thisHref+'?w=450" width="450" class="mshot-image" style="margin: 0;" /></div>');
-			setTimeout(function () {
-				self.find( '.mshot-image' ).attr('src', '//s0.wordpress.com/mshots/v1/'+thisHref+'?w=450&r=2');
-			}, 6000);
-			setTimeout(function () {
-				self.find( '.mshot-image' ).attr('src', '//s0.wordpress.com/mshots/v1/'+thisHref+'?w=450&r=3');
-			}, 12000);
-		} else {
-			$(this).find('.mShot').css('left', thisParentWidth).show();
+
+	// Show a preview image of the hovered URL. Applies to author URLs and URLs inside the comments.
+	$( '#the-comment-list' ).on( 'mouseover', 'a[id^="author_comment_url"], tr.pingback td.column-author a:first-of-type, td.comment p a', function () {
+		clearTimeout( mshotRemovalTimer );
+
+		if ( $( '.akismet-mshot' ).length > 0 ) {
+			if ( $( '.akismet-mshot:first' ).data( 'link' ) == this ) {
+				// The preview is already showing for this link.
+				return;
+			}
+			else {
+				// A new link is being hovered, so remove the old preview.
+				$( '.akismet-mshot' ).remove();
+			}
 		}
-	}).mouseout(function () {
-		$(this).find('.mShot').hide();
-	});
+
+		clearTimeout( mshotSecondTryTimer );
+		clearTimeout( mshotThirdTryTimer );
+
+		var thisHref = encodeURIComponent( $( this ).attr( 'href' ) );
+
+		var mShot = $( '<div class="akismet-mshot mshot-container"><div class="mshot-arrow"></div><img src="//s0.wordpress.com/mshots/v1/' + thisHref + '?w=900" width="450" height="338" class="mshot-image" /></div>' );
+		mShot.data( 'link', this );
+
+		var offset = $( this ).offset();
+
+		mShot.offset( {
+			left : Math.min( $( window ).width() - 475, offset.left + $( this ).width() + 10 ), // Keep it on the screen if the link is near the edge of the window.
+			top: offset.top + ( $( this ).height() / 2 ) - 101 // 101 = top offset of the arrow plus the top border thickness
+		} );
+
+		// These retries appear to be superfluous if .mshot-image has already loaded, but it's because mShots
+		// can return a "Generating thumbnail..." image if it doesn't have a thumbnail ready, so we need
+		// to retry to see if we can get the newly generated thumbnail.
+		mshotSecondTryTimer = setTimeout( function () {
+			mShot.find( '.mshot-image' ).attr( 'src', '//s0.wordpress.com/mshots/v1/'+thisHref+'?w=900&r=2' );
+		}, 6000 );
+
+		mshotThirdTryTimer = setTimeout( function () {
+			mShot.find( '.mshot-image' ).attr( 'src', '//s0.wordpress.com/mshots/v1/'+thisHref+'?w=900&r=3' );
+		}, 12000 );
+
+		$( 'body' ).append( mShot );
+	} ).on( 'mouseout', 'a[id^="author_comment_url"], tr.pingback td.column-author a:first-of-type, td.comment p a', function () {
+		mshotRemovalTimer = setTimeout( function () {
+			clearTimeout( mshotSecondTryTimer );
+			clearTimeout( mshotThirdTryTimer );
+
+			$( '.akismet-mshot' ).remove();
+		}, 200 );
+	} );
+
 	$('.checkforspam:not(.button-disabled)').click( function(e) {
-		$('.checkforspam:not(.button-disabled)').addClass('button-disabled');
-		$('.checkforspam-spinner').addClass( 'spinner' );
-		akismet_check_for_spam(0, 100);
 		e.preventDefault();
+
+		$('.checkforspam:not(.button-disabled)').addClass('button-disabled');
+		$('.checkforspam-spinner').addClass( 'spinner' ).addClass( 'is-active' );
+
+		// Update the label on the "Check for Spam" button to use the active "Checking for Spam" language.
+		$( '.checkforspam .akismet-label' ).text( $( '.checkforspam' ).data( 'active-label' ) );
+
+		akismet_check_for_spam(0, 100);
 	});
 
+	var spam_count = 0;
+	var recheck_count = 0;
+
 	function akismet_check_for_spam(offset, limit) {
+		// Update the progress counter on the "Check for Spam" button.
+		$( '.checkforspam-progress' ).text( $( '.checkforspam' ).data( 'progress-label-format' ).replace( '%1$s', offset ) );
+
 		$.post(
 			ajaxurl,
 			{
@@ -139,20 +159,71 @@ jQuery( function ( $ ) {
 				'limit': limit
 			},
 			function(result) {
-				if (result.processed < limit) {
-					window.location.reload();
+				recheck_count += result.counts.processed;
+				spam_count += result.counts.spam;
+				
+				if (result.counts.processed < limit) {
+					window.location.href = $( '.checkforspam' ).data( 'success-url' ).replace( '__recheck_count__', recheck_count ).replace( '__spam_count__', spam_count );
 				}
 				else {
-					akismet_check_for_spam(offset + limit, limit);
+					// Account for comments that were caught as spam and moved out of the queue.
+					akismet_check_for_spam(offset + limit - result.counts.spam, limit);
 				}
 			}
 		);
 	}
-});
-// URL encode plugin
-jQuery.extend({URLEncode:function(c){var o='';var x=0;c=c.toString();var r=/(^[a-zA-Z0-9_.]*)/;
-  while(x<c.length){var m=r.exec(c.substr(x));
-    if(m!=null && m.length>1 && m[1]!=''){o+=m[1];x+=m[1].length;
-    }else{if(c[x]==' ')o+='+';else{var d=c.charCodeAt(x);var h=d.toString(16);
-    o+='%'+(h.length<2?'0':'')+h.toUpperCase();}x++;}}return o;}
+	
+	if ( "start_recheck" in WPAkismet && WPAkismet.start_recheck ) {
+		$( '.checkforspam' ).click();
+	}
+	
+	if ( typeof MutationObserver !== 'undefined' ) {
+		// Dynamically add the "X" next the the author URL links when a comment is quick-edited.
+		var comment_list_container = document.getElementById( 'the-comment-list' );
+
+		if ( comment_list_container ) {
+			var observer = new MutationObserver( function ( mutations ) {
+				for ( var i = 0, _len = mutations.length; i < _len; i++ ) {
+					if ( mutations[i].addedNodes.length > 0 ) {
+						akismet_enable_comment_author_url_removal();
+						
+						// Once we know that we'll have to check for new author links, skip the rest of the mutations.
+						break;
+					}
+				}
+			} );
+			
+			observer.observe( comment_list_container, { attributes: true, childList: true, characterData: true } );
+		}
+	}
+
+	function akismet_enable_comment_author_url_removal() {
+		$( '#the-comment-list' )
+			.find( 'tr.comment, tr[id ^= "comment-"]' )
+			.find( '.column-author a[href^="http"]:first' ) // Ignore mailto: links, which would be the comment author's email.
+			.each(function () {
+				if ( $( this ).parent().find( '.akismet_remove_url' ).length > 0 ) {
+					return;
+				}
+			
+			var linkHref = $(this).attr( 'href' );
+		
+			// Ignore any links to the current domain, which are diagnostic tools, like the IP address link
+			// or any other links another plugin might add.
+			var currentHostParts = document.location.href.split( '/' );
+			var currentHost = currentHostParts[0] + '//' + currentHostParts[2] + '/';
+		
+			if ( linkHref.indexOf( currentHost ) != 0 ) {
+				var thisCommentId = $(this).parents('tr:first').attr('id').split("-");
+
+				$(this)
+					.attr("id", "author_comment_url_"+ thisCommentId[1])
+					.after(
+						$( '<a href="#" class="akismet_remove_url">x</a>' )
+							.attr( 'commentid', thisCommentId[1] )
+							.attr( 'title', WPAkismet.strings['Remove this URL'] )
+					);
+			}
+		});
+	}
 });
---
+++ wp-content/themes/wordpress-theme-dev/functions.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ * wordpress theme dev functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package wordpress_theme_dev
+ */
+
+if ( ! function_exists( 'wordpress_theme_dev_setup' ) ) :
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
+	 */
+	function wordpress_theme_dev_setup() {
+		/*
+		 * Make theme available for translation.
+		 * Translations can be filed in the /languages/ directory.
+		 * If you're building a theme based on wordpress theme dev, use a find and replace
+		 * to change 'wordpress-theme-dev' to the name of your theme in all the template files.
+		 */
+		load_theme_textdomain( 'wordpress-theme-dev', get_template_directory() . '/languages' );
+
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
+
+		/*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support( 'title-tag' );
+
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+		add_theme_support( 'post-thumbnails' );
+
+		// This theme uses wp_nav_menu() in one location.
+		register_nav_menus( array(
+			'menu-1' => esc_html__( 'Primary', 'wordpress-theme-dev' ),
+		) );
+
+		/*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+		add_theme_support( 'html5', array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+		) );
+
+		// Set up the WordPress core custom background feature.
+		add_theme_support( 'custom-background', apply_filters( 'wordpress_theme_dev_custom_background_args', array(
+			'default-color' => 'ffffff',
+			'default-image' => '',
+		) ) );
+
+		// Add theme support for selective refresh for widgets.
+		add_theme_support( 'customize-selective-refresh-widgets' );
+
+		/**
+		 * Add support for core custom logo.
+		 *
+		 * @link https://codex.wordpress.org/Theme_Logo
+		 */
+		add_theme_support( 'custom-logo', array(
+			'height'      => 250,
+			'width'       => 250,
+			'flex-width'  => true,
+			'flex-height' => true,
+		) );
+	}
+endif;
+add_action( 'after_setup_theme', 'wordpress_theme_dev_setup' );
+
+/**
+ * Set the content width in pixels, based on the theme's design and stylesheet.
+ *
+ * Priority 0 to make it available to lower priority callbacks.
+ *
+ * @global int $content_width
+ */
+function wordpress_theme_dev_content_width() {
+	$GLOBALS['content_width'] = apply_filters( 'wordpress_theme_dev_content_width', 640 );
+}
+add_action( 'after_setup_theme', 'wordpress_theme_dev_content_width', 0 );
+
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function wordpress_theme_dev_widgets_init() {
+	register_sidebar( array(
+		'name'          => esc_html__( 'Sidebar', 'wordpress-theme-dev' ),
+		'id'            => 'sidebar-1',
+		'description'   => esc_html__( 'Add widgets here.', 'wordpress-theme-dev' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+}
+add_action( 'widgets_init', 'wordpress_theme_dev_widgets_init' );
+
+/**
+ * Enqueue scripts and styles.
+ */
+function wordpress_theme_dev_scripts() {
+	wp_enqueue_style( 'wordpress-theme-dev-style', get_stylesheet_uri() );
+
+	wp_enqueue_script( 'wordpress-theme-dev-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+
+	wp_enqueue_script( 'wordpress-theme-dev-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'wordpress_theme_dev_scripts' );
+
+/**
+ * Implement the Custom Header feature.
+ */
+require get_template_directory() . '/inc/custom-header.php';
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Functions which enhance the theme by hooking into WordPress.
+ */
+require get_template_directory() . '/inc/template-functions.php';
+
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/inc/customizer.php';
+
+/**
+ * Load Jetpack compatibility file.
+ */
+if ( defined( 'JETPACK__VERSION' ) ) {
+	require get_template_directory() . '/inc/jetpack.php';
+}
