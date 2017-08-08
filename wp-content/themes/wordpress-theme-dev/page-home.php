302ce33b089cab0bd1390b240b9585babf1cf148
Wasid Hossain <wasidmail@gmail.com>
2017-08-08 07:03:00 +0000

    Code the Boost Your Income Section with Advanced Custom Fields

--- wp-content/themes/wordpress-theme-dev/page-home.php
+++ wp-content/themes/wordpress-theme-dev/page-home.php
@@ -2,6 +2,7 @@
 /*
     Template Name: Home Page
 */
+    // Custom Fields 
     $pre_launch_price   = get_post_meta(12, pre_launch_price, true);
     $launch_price       = get_post_meta(12, launch_price, true);
     $final_price        = get_post_meta(12, final_price, true);
@@ -9,6 +10,16 @@
     $button_text        = get_post_meta(12, button_text, true);
     $optin_text         = get_post_meta(12, optin_text, true);
     $optin_button_text  = get_post_meta(12, optin_button_text, true);
+    
+    // Advanced Custom Fields
+    
+    $income_feature_image       = get_field('income_feature_image');
+    $income_section_title       = get_field('income_section_title');
+    $income_section_description = get_field('income_section_description');
+    $reason_1_title             = get_field('reason_1_title');
+    $reason_1_description       = get_field('reason_1_description');
+    $reason_2_title             = get_field('reason_2_title');
+    $reason_2_description       = get_field('reason_2_description');
 
 get_header(); ?>
 
@@ -74,23 +85,26 @@ get_header(); ?>
     <section id="boost-income">
         <div class="container">
             <div class="section-header">
-                <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-boost.png" att="Chart">
-                <h2>How You Can Boots Your Income</h2>
+                
+                <?php if( !empty($income_feature_image) ) : ?>
+                    
+                    <img src="<?php echo $income_feature_image['url']; ?>" att="<?php echo $income_feature_image['alt']; ?>">
+                
+                <?php endif; ?>
+                
+                <h2><?php echo $income_section_title; ?></h2>
             </div>
             <p class="lead">
-                Whether you're a freelance designer, entrepreneur, employee for a company, code hobbyist, or looking for a new career — this course gives you an immensely valuable skill that will enable you to either:
+                <?php echo $income_section_description; ?>
             </p>
             <div class="row">
                 <div class="col-sm-6">
-                    <h3>Make money on the side</h3>
-                    <p>So you can save up for that Hawaiian vacation you’ve been wanting, help pay off your debt, your car, your mortgage, or simply just to have bonus cash laying around
-                    </p>
+                    <h3><?php echo $reason_1_title; ?></h3>
+                    <p><?php echo $reason_1_description; ?></p>
                 </div>
                 <div class="col-sm-6">
-                    <h3>Create a full-time income</h3>
-                    <p>
-                       WordPress developers have options. Many developers make a generous living off of creating custom WordPress themes and selling them on websites like ThemeForest. Freelance designers and developers can also take on WordPress projects and make an extra $1,000 - $5,000+ per month.
-                    </p>
+                    <h3><?php echo $reason_2_title; ?></h3>
+                    <p><?php echo $reason_2_description; ?></p>
                 </div>
             </div>
         </div>
