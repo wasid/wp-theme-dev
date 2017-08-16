208600377a0e13f8521857fdc53135de471fd73d
Wasid Hossain <wasidmail@gmail.com>
2017-08-16 09:38:36 +0000

    Added a Resource and Display It In Custom Theme

--- wp-content/themes/wordpress-theme-dev/page-resources.php
+++ wp-content/themes/wordpress-theme-dev/page-resources.php
@@ -32,36 +32,35 @@ $thumbnail_url = wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) );
             <div class="row" id="primary">
                 <div id="content" class="col-sm-12">
                     <section class="main-content">
-                        <p class="lead">
-                            Welcome to the Bootstrap to WordPress resources page! On this page, I will provide you with useful resources, plugins, tools, applications, and websites to help make your life as a coder a little bit easier. <strong>I recommend adding this page to your bookmarks for convenient future reference.
-                        </strong>
-                        </p>
-                        <p><em>
-                            Please keep in mind that some of the following resources are my affiliate links, and should you decide to make a purchase, I will earn a small commission &mdash; at no extra cost to you! I am recommending these resources only because I have found them to be incredibly helpful. Don&rsquo;t spend your money on any products or resources unless you think they will benefit you.
-                        </em></p>
+                         <?php while( have_posts() ) : the_post(); ?>
+                             
+                             <?php the_content(); ?>
+                         
+                         <?php endwhile; ?>
                         <hr>
                         <div class="resource-row clearfix">
+                        <?php $loop = new WP_Query( array( 'post_type' => 'course_resources', 'orderby' => 'post_id', 'order' => 'ASC'  )); ?>
+                
+                        <?php while( $loop->have_posts() ) : $loop->the_post(); ?>
+                        
+                        <?php
+                            $resource_image = get_field('resource_image');
+                            $resource_url   = get_field('resource_url');
+                            $button_text    = get_field('button_text');
+                        ?>
 						<div class="resource">
-							<img src="http://bootstraptowordpress.com/wp-content/uploads/2014/12/justhost-logo.jpg" alt="Justhost Logo">
-							<h3><a href="#">Justhost</a></h3>
-							<p>When I made my very first website about 7 years ago, I signed up with Justhost — and I’ve never looked back! Since then, I’ve built hundreds of websites with Justhost as my only hosting provider. I recommend these guys to all of my clients, my students, my family and friends.</p>
-                            <p>If you’re looking to start your very first website, or are looking for a reliable hosting provider — look no further. You get a free domain name for life, great customer service, unlimited domains, unlimited email accounts, unlimited GBs of space, an anytime money back guarantee and my favourite part: You can set up a blog website in minutes with 1-click WordPress Install. These guys are awesome! Get started with Justhost.</p>
-				    		<a href="#" class="btn btn-success">Get started with Justhost</a>
+							<img src="<?php echo $resource_image[url]; ?>" alt="<?php echo $resource_image[alt]; ?>">
+							
+							<h3><a href="<?php echo $resource_url; ?>"><?php the_title(); ?></a></h3>
+							     
+							     <?php the_content(); ?>
+							     
+				    		<?php if (!empty($button_text)) { ?>
+				    		    <a href="<?php echo $resource_url ?>" class="btn btn-success"><?php echo $button_text ?></a>
+				    		<?php } ?>
+				    		
 						</div><!-- resource -->
-						<div class="resource">
-    						<img src="http://bootstraptowordpress.com/wp-content/uploads/2014/12/coda2-logo.jpg" alt="Coda 2 Logo">
-							<h3><a href="#">Coda 2</a></h3>
-							<p>If you’re serious about coding, or you want to start out with a solid tool to code with, I strongly recommend Coda 2 for Mac. A beautiful User Interface with tabbed navigation, a super sidebar, and a simple layout. The features are outrageous and extensive, with code folding, smart complete, auto indentation, GIT integration, a built-in terminal, iCloud support — to name a few. The features seem to be endless.</p>
-                            <p>I use Coda 2 for all of my courses &amp; tutorials, and if you decide to use Coda 2 as your editor of choice, it would greatly benefit you when taking my courses. The price of the editor weighs in at $75, but it’s worth every penny — in my honest opinion.</p>
-							<a href="#" class="btn btn-success">Check out Coda 2</a>
-						</div><!-- resource -->
-						<div class="resource">
-							<img src="http://bootstraptowordpress.com/wp-content/uploads/2014/12/dropbox-logo.jpg" alt="Dropbox Logo">
-							<h3><a href="#">Dropbox</a></h3>
-							<p>This tool should go without saying. But if I have to explain — you <strong>need</strong> this tool. I use it 24/7 — literally. Dropbox is where I store my personal &amp; business related data. It’s accessible anywhere, and regularly backs up my important information.</p>
-                            <p>Because my data is accessible anywhere, Dropbox allows me to work on a client project at home, on the bus, in a coffee shop, or at a friend’s house. It’s truly a gem of a tool, and I think you’ll believe me.</p>
-                            <a href="#" class="btn btn-success">Get 2GB in Dropbox for free</a>
-					    </div><!-- resource -->
+				        <?php endwhile; wp_reset_query(); ?>
 					</div>
                     </section>
                 </div>
