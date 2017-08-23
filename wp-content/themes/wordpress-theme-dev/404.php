7484e8cce26a163ccc574dd4145590ccab56defd
Wasid Hossain <wasidmail@gmail.com>
2017-08-23 11:29:41 +0000

    Created a Useful & Engaging 404 Page

--- wp-content/themes/wordpress-theme-dev/404.php
+++ wp-content/themes/wordpress-theme-dev/404.php
@@ -9,25 +9,51 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
+        <h1 class="page-title">404! page not found!</h1>
+    </section>
+        <!--Blog Content-->
 
-			<section class="error-404 not-found">
+    <div class="container">
+        <div class="row" id="primary">
+            <main id="content" class="col-sm-8" role="main">
+
+			<div class="error-404 not-found">
 				<header class="page-header">
 					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'wordpress-theme-dev' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'wordpress-theme-dev' ); ?></p>
+					<h3>Resouces</h3>
+					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the resouces below?', 'wordpress-theme-dev' ); ?></p>
 
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-					?>
+					    <div class="resource-row clearfix">
+                        <?php $loop = new WP_Query( array( 'post_type' => 'course_resources', 'orderby' => 'post_id', 'order' => 'ASC'  )); ?>
+                
+                        <?php while( $loop->have_posts() ) : $loop->the_post(); ?>
+                        
+                        <?php
+                            $resource_image = get_field('resource_image');
+                            $resource_url   = get_field('resource_url');
+                            $button_text    = get_field('button_text');
+                        ?>
+    						<div class="resource">
+    							<img src="<?php echo $resource_image[url]; ?>" alt="<?php echo $resource_image[alt]; ?>">
+    							
+    							<h3><a href="<?php echo $resource_url; ?>"><?php the_title(); ?></a></h3>
+    							     
+    							     <?php the_content(); ?>
+    							     
+    				    		<?php if (!empty($button_text)) { ?>
+    				    		    <a href="<?php echo $resource_url ?>" class="btn btn-success"><?php echo $button_text ?></a>
+    				    		<?php } ?>
+    				    		
+    						</div><!-- resource -->
+				        <?php endwhile; wp_reset_query(); ?>
+					    </div>
 
 					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'wordpress-theme-dev' ); ?></h2>
+						<h4 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'wordpress-theme-dev' ); ?></h4>
 						<ul>
 						<?php
 							wp_list_categories( array(
@@ -40,21 +66,25 @@ get_header(); ?>
 						?>
 						</ul>
 					</div><!-- .widget -->
-
 					<?php
 
 						/* translators: %1$s: smiley */
 						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'wordpress-theme-dev' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+						the_widget( 'WP_Widget_Archives', 'title=Our Archives', "before_title=<h4 class='widgettitle'>&after_title=</h4>$archive_content" );
 
-						the_widget( 'WP_Widget_Tag_Cloud' );
+						the_widget( 'WP_Widget_Tag_Cloud', 'title=Our Tags', "before_title=<h4 class='widgettitle'>&after_title=</h4>" );
+						
 					?>
-
+					<p>.....or, just go back to the <a href="<?php echo esc_url(home_url('/')); ?>">Home Page</a></p>
 				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
+			</div><!-- .error-404 -->
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+			</main><!-- #main -->
+			<aside class="col-sm-4">
+	        	<?php get_sidebar(); ?>	
+	        </aside><!-- sidebar -->
+		</div><!-- #primary -->
+	</div><!-- container -->
 
 <?php
 get_footer();
--- wp-content/themes/wordpress-theme-dev/page-resources.php
+++ wp-content/themes/wordpress-theme-dev/page-resources.php
@@ -48,20 +48,20 @@ $thumbnail_url = wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) );
                             $resource_url   = get_field('resource_url');
                             $button_text    = get_field('button_text');
                         ?>
-						<div class="resource">
-							<img src="<?php echo $resource_image[url]; ?>" alt="<?php echo $resource_image[alt]; ?>">
-							
-							<h3><a href="<?php echo $resource_url; ?>"><?php the_title(); ?></a></h3>
-							     
-							     <?php the_content(); ?>
-							     
-				    		<?php if (!empty($button_text)) { ?>
-				    		    <a href="<?php echo $resource_url ?>" class="btn btn-success"><?php echo $button_text ?></a>
-				    		<?php } ?>
-				    		
-						</div><!-- resource -->
+    						<div class="resource">
+    							<img src="<?php echo $resource_image[url]; ?>" alt="<?php echo $resource_image[alt]; ?>">
+    							
+    							<h3><a href="<?php echo $resource_url; ?>"><?php the_title(); ?></a></h3>
+    							     
+    							     <?php the_content(); ?>
+    							     
+    				    		<?php if (!empty($button_text)) { ?>
+    				    		    <a href="<?php echo $resource_url ?>" class="btn btn-success"><?php echo $button_text ?></a>
+    				    		<?php } ?>
+    				    		
+    						</div><!-- resource -->
 				        <?php endwhile; wp_reset_query(); ?>
-					</div>
+					    </div>
                     </section>
                 </div>
             </div>
